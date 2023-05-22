a88a4a55a62520b5dc5b0e7926a185ce34c6c6da
Arsh Suri <gunmala@gunmalas-MacBook-Pro.local>
2023-05-22 17:24:31 +0530

    corrected JoinRoom handler in ws_handler

--- server/internal/ws/client.go
+++ server/internal/ws/client.go
@@ -1,6 +1,10 @@
 package ws
 
-import "github.com/gorilla/websocket"
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
 	Conn     *websocket.Conn
@@ -14,4 +18,39 @@ type Message struct {
 	Username string `json:"Username"`
 	RoomID   string `json:"RoomID"`
 	Content  string `json:"Content"`
+}
+
+func (c *Client) writeMessage() {
+	defer func() {
+		c.Conn.Close()
+	}()
+	for {
+		message, ok := <-c.Message
+		if !ok {
+			return
+		}
+		c.Conn.WriteJSON(message)
+	}
+}
+
+func (c *Client) readMessage(h *Hub) {
+	defer func() {
+		h.Unregister <- c
+		c.Conn.Close()
+	}()
+	for {
+		_, m, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		msg := &Message{
+			Content:  string(m),
+			Username: c.Username,
+			RoomID:   c.RoomID,
+		}
+		h.Broadcast <- msg
+	}
 }
